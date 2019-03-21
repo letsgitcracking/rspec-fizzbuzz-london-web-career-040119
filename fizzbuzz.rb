1a09900eb6504a41c16e89835cb39721005f47c3
_ _ <mkiyani007@gmail.com>
2019-03-21 17:20:21 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,6 +5,9 @@ def fizzbuzz(num)
     puts 'Fizz'
   elsif num % 5 == 0 
     puts 'Buzz'
-  else
-    num % 3 == 0 
+  elsif
+    num % 3 == 0 %% num % 5 == 0
     puts 'Fizz'
+  else
+    puts
+    
