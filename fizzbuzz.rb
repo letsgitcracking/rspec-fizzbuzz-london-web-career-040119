13b4b08b00d9710d6b5a46abb643aeb6c1f4f614
_ _ <mkiyani007@gmail.com>
2019-03-21 17:23:44 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,12 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
   if num % 3 == 0 
-    puts 'Fizz'
+    'Fizz'
   elsif num % 5 == 0 
-    puts 'Buzz'
+    'Buzz'
   elsif
     num % 3 == 0 && num % 5 == 0
-    puts 'FizzBuzz'
+    'FizzBuzz'
   else
 end
     
