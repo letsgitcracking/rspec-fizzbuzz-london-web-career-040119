063c615b0d421615525ea4d8a7c8db02d63c98c5
_ _ <mkiyani007@gmail.com>
2019-03-21 17:42:27 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-    
+def fizzbuzz num 
+  if num % 3 == 10 
+    "Fizz"
+  end
+end
